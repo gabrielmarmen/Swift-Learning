44e1853e3d8e206a4ccd87d916641151113e19a3
Gabriel Marmen <Gabriel Marmen>
2022-07-28 11:05:34 +0800

    Simple Variable Types

---
+++ Part 1 to 12/Simple Variable Types.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+//Declaring variables with different types
+var greeting = "Hello, playground"
+greeting = "Sheesh"
+var age = 18
+
+//Multilines Strings
+var str1 = """
+Sheesh
+On
+Multiple lines
+"""
+//String formated only in code
+var str2 = """
+Sheesh formated \
+on multiple lines \
+only in code
+"""
+
+//Creates a bool
+var IsTrue = true
+
+//Creates a double
+var double = 4.20
+
+//This isert a value into a string
+var str3 = "Your score was \(double)"
+
+//Constant declaration
+let sheesh = "SHEEESH"
+
+//Force a type on variables (Type Annotation)
+var str6: String = "This is a string"
+var height: Double = 1.80
+var IsTall: Bool = true
+var Age: Int = 5
+
+
+
+
