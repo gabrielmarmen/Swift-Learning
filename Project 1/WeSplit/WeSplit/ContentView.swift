bc9a1e6bf3d4eecb0d4d8f7f0abbb814a1578d5d
Gabriel Marmen <gabton@outlook.com>
2022-08-01 17:48:26 +0800

    Started first project

---
+++ Project 1/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Gabriel Marmen on 2022-08-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form{
+            Section{
+                Text("Hello, word!")
+                Text("Hello, word!")
+                Text("Hello, word!")
+            }
+            
+            Section{
+                Text("Hello, word!")
+            }
+            
+            
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project 1/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Gabriel Marmen on 2022-08-01.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
