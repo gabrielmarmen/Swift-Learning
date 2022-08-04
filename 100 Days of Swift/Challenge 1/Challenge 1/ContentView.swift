36d43c38a0afcc174ebb1d6c55a28790040f63b6
Gabriel Marmen <gabton@outlook.com>
2022-08-04 11:27:52 +0800

    Finished Challenge 1 + refining Challenge 1

--- 100 Days of Swift/Challenge 1/Challenge 1/ContentView.swift
+++ 100 Days of Swift/Challenge 1/Challenge 1/ContentView.swift
@@ -8,9 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var input: String = ""
+    @State private var selectedInputMeasurement: UnitLength = UnitLength.kilometers
+    @State private var selectedOutputMeasurement: UnitLength = UnitLength.miles
+    
+    private var inputDouble: Double{
+        Double(input) ?? 0.0
+    }
+    private var measurementInput: Measurement<UnitLength> {
+        return Measurement(value: inputDouble, unit: selectedInputMeasurement)
+    }
+    private var measurementOutputString: String {
+        return String((round(measurementInput.converted(to: selectedOutputMeasurement).value * 100)/100).formatted()) + " " + selectedOutputMeasurement.symbol
+    }
+    
+    
+    
+    private let measurementUnits: [UnitLength] = [UnitLength.kilometers, UnitLength.miles, UnitLength.feet, UnitLength.centimeters]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Value", text: $input)
+                        .keyboardType(.decimalPad)
+                    Picker("Input Measurement", selection: $selectedInputMeasurement) {
+                        ForEach(measurementUnits, id: \.self){ value in
+                            Text(value.symbol)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    
+                } header: {
+                    Text("Convert")
+                }
+                
+                Section{
+                    Picker("Input Measurement", selection: $selectedOutputMeasurement) {
+                        ForEach(measurementUnits, id: \.self){ value in
+                            Text(value.symbol)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    Text(measurementOutputString)
+                    
+                } header: {
+                    Text("To")
+                }
+            }
+            .navigationTitle("Unit Converter")
+        }
+        
+            
+        
     }
 }
 
