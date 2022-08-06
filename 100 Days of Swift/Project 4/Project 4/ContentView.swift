8b4f84328a094337502ead780be67726d522c3d1
Gabriel Marmen <gabton@outlook.com>
2022-08-06 20:30:50 +0800

    Created  Project 4

---
+++ 100 Days of Swift/Project 4/Project 4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project 4
+//
+//  Created by Gabriel Marmen on 2022-08-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 100 Days of Swift/Project 4/Project 4/Project_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_4App.swift
+//  Project 4
+//
+//  Created by Gabriel Marmen on 2022-08-06.
+//
+
+import SwiftUI
+
+@main
+struct Project_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
