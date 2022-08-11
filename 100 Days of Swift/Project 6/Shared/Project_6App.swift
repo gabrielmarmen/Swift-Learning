d3cc3d22c1b15fb1ebfaa7c2fd1c1aa61eed62de
Gabriel Marmen <gabton@outlook.com>
2022-08-11 12:40:33 +0800

    Created Project 5

---
+++ 100 Days of Swift/Project 6/Shared/Project_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_6App.swift
+//  Shared
+//
+//  Created by Gabriel Marmen on 2022-08-11.
+//
+
+import SwiftUI
+
+@main
+struct Project_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
