ca27f695785f7009a93536115da25a2c3f2a6471
Gabriel Marmen <gabton@outlook.com>
2022-08-13 18:50:05 +0800

    Added project 7

---
+++ 100 Days of Swift/Project 7/Shared/Project_7App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_7App.swift
+//  Shared
+//
+//  Created by Gabriel Marmen on 2022-08-13.
+//
+
+import SwiftUI
+
+@main
+struct Project_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
