592411ff00a98f590051f6b3d6dc557975a059e1
Gabriel Marmen <gabton@outlook.com>
2022-08-14 21:42:16 +0800

    Wiped project 7

--- 100 Days of Swift/Project 7/Shared/ContentView.swift
+++ 100 Days of Swift/Project 7/Shared/ContentView.swift
@@ -10,11 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     
-    
     var body: some View {
-        
+        Text("")
+    }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
