eede677789bc921e91e8cc2f8c446749c2e1e5af
Gabriel Marmen <gabton@outlook.com>
2022-08-18 21:37:54 +0800

    Continued Project 8

---
+++ 100 Days of Swift/Project 8/Shared/MissionView.swift
@@ -0,0 +1,20 @@
+//
+//  MissionView.swift
+//  Project 8
+//
+//  Created by Gabriel Marmen on 2022-08-18.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    static var previews: some View {
+        MissionView()
+    }
+}
