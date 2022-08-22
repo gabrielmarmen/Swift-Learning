b759a72804bd32b90d3a9f32f8d40d3c0d0e6de9
Gabriel Marmen <gabton@outlook.com>
2022-08-22 13:14:42 +0800

    Finished Project 8 with Challenge

---
+++ 100 Days of Swift/Project 8/Shared/MissionHighlightsView.swift
@@ -0,0 +1,20 @@
+//
+//  MissionHighlightsView.swift
+//  Project 8 (iOS)
+//
+//  Created by Gabriel Marmen on 2022-08-22.
+//
+
+import SwiftUI
+
+struct MissionHighlightsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MissionHighlightsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MissionHighlightsView()
+    }
+}
