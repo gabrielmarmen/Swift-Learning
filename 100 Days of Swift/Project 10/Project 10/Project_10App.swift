89b4bdd029e50ad9b0f4dc38aca057fe0125575d
Gabriel Marmen <gabton@outlook.com>
2022-08-26 21:47:03 +0800

    Half of project 10

---
+++ 100 Days of Swift/Project 10/Project 10/Project_10App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_10App.swift
+//  Project 10
+//
+//  Created by Gabriel Marmen on 2022-08-26.
+//
+
+import SwiftUI
+
+@main
+struct Project_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
