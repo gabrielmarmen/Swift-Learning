52797922892693ed2e669dcbb1b48864c16c4d5a
Gabriel Marmen <gabton@outlook.com>
2022-08-28 13:42:36 +0800

    Added files that were forgotten in last commit

--- 100 Days of Swift/Project 10/Project 10/Order.swift
+++ 100 Days of Swift/Project 10/Project 10/Order.swift
@@ -6,3 +6,70 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct Order: Codable {
+    
+    enum CodingKeys: CodingKey {
+        case type, quantity, extraFrosting, addSprinkles, name, streetAddress, city, zip
+    }
+    
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+
+    var type = 0
+    var quantity = 3
+
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    
+    var extraFrosting = false
+    var addSprinkles = false
+    
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
+    init() { }
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        
+        if name.isBlank || streetAddress.isBlank || city.isBlank || zip.isBlank
+        {
+            return false
+        }
+
+        return true
+    }
+    
+    
+    var cost: Double {
+        // $2 per cake
+        var cost = Double(quantity) * 2
+
+        // complicated cakes cost more
+        cost += (Double(type) / 2)
+
+        // $1/cake for extra frosting
+        if extraFrosting {
+            cost += Double(quantity)
+        }
+
+        // $0.50/cake for sprinkles
+        if addSprinkles {
+            cost += Double(quantity) / 2
+        }
+
+        return cost
+    }
+    
+}
--- 100 Days of Swift/Project 10/Project 10/Orders.swift
+++ 100 Days of Swift/Project 10/Project 10/Orders.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class Orders : Codable, ObservableObject {
+    
+    enum CodingKeys: CodingKey {
+        case order
+    }
+    
+    @Published var order = Order()
+    
+    
+    init() {}
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        order = try container.decode(Order.self, forKey: .order)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(order, forKey: .order)
+    }
+    
+    
+}
