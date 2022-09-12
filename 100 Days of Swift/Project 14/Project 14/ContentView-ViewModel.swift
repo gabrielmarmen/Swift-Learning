c14398bd3d274854e6c3fed65587c4e4b7b2fe4f
Gabriel Marmen <gabton@outlook.com>
2022-09-12 19:33:59 +0700

    Finished Project 14 app

--- 100 Days of Swift/Project 14/Project 14/ContentView-ViewModel.swift
+++ 100 Days of Swift/Project 14/Project 14/ContentView-ViewModel.swift
@@ -7,12 +7,16 @@
 
 import Foundation
 import MapKit
+import LocalAuthentication
 
 extension ContentView {
     @MainActor class ViewModel: ObservableObject {
+        
+        
         @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
         @Published private(set) var locations: [Location]
         @Published var selectedPlace: Location?
+        @Published var isUnlocked = false
         let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
         
         init() {
@@ -23,6 +27,7 @@ extension ContentView {
                 locations = []
             }
         }
+        
         func addLocation() {
             let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
             locations.append(newLocation)
@@ -37,12 +42,35 @@ extension ContentView {
             }
             save()
         }
+        
         func save() {
             do {
                 let data = try JSONEncoder().encode(locations)
                 try data.write(to: savePath, options: [.atomic, .completeFileProtection])
             } catch {
                 print("Unable to save data.")
+            }
+        }
+        
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Please authenticate yourself to unlock your places."
+
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+
+                    if success {
+                        Task { @MainActor in
+                                self.isUnlocked = true
+                            }
+                    } else {
+                        // error
+                    }
+                }
+            } else {
+                // no biometrics
             }
         }
     }
