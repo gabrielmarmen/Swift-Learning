02fed5fdc27d60b97f4e6f2713111e7e09d186ce
Gabriel Marmen <gabton@outlook.com>
2022-09-19 21:06:57 +0700

    Created Challenge 7. Read wrapup for Challenge 7.

---
+++ 100 Days of Swift/Challenge 6/Challenge 6/Challenge_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_6App.swift
+//  Challenge 6
+//
+//  Created by Gabriel Marmen on 2022-09-19.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
