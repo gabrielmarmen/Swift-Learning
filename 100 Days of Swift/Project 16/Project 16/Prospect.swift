107ff78edff2b272dfe70a7651946dbcd4edb35e
Gabriel Marmen <gabton@outlook.com>
2022-09-27 23:22:12 +0700

    test

---
+++ 100 Days of Swift/Project 16/Project 16/Prospect.swift
@@ -0,0 +1,23 @@
+//
+//  Prospect.swift
+//  Project 16
+//
+//  Created by Gabriel Marmen on 2022-09-27.
+//
+
+import Foundation
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    var isContacted = false
+}
+
+@MainActor class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+
+    init() {
+        self.people = []
+    }
+}
