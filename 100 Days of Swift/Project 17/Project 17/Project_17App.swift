16b2f90bed7d335eee9c2e66c39a9d6d99bec91a
Gabriel Marmen <gabton@outlook.com>
2022-10-03 22:44:01 +0700

    Day 1 of Project 17

---
+++ 100 Days of Swift/Project 17/Project 17/Project_17App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_17App.swift
+//  Project 17
+//
+//  Created by Gabriel Marmen on 2022-10-03.
+//
+
+import SwiftUI
+
+@main
+struct Project_17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
