b8b7106dad98225858a4c9afdcbbd87e34858cf4
Gabriel Marmen <gabton@outlook.com>
2022-10-04 21:39:35 +0700

    Completed Project 17. Challenge to be done

---
+++ 100 Days of Swift/Project 17/Project 17/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  Project 17
+//
+//  Created by Gabriel Marmen on 2022-10-04.
+//
+
+import Foundation
---
+++ 100 Days of Swift/Project 17/Project 17/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  Project 17
+//
+//  Created by Gabriel Marmen on 2022-10-04.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
