ca0211ec393c13cc09685bdef5a43d4b3d141fce
Gabriel Marmen <gabton@outlook.com>
2022-10-05 19:24:06 +0700

    Created Project 18 and finished part 1

---
+++ 100 Days of Swift/Project 18/Project 18/Project_18App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_18App.swift
+//  Project 18
+//
+//  Created by Gabriel Marmen on 2022-10-05.
+//
+
+import SwiftUI
+
+@main
+struct Project_18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
